534420f0e8df6055d0eaba35b9ebb875ee655c38
unknown <freetownstepper@gmail.com>
2026-02-22 10:19:45 +0100

    sass project setup

---
+++ gulp-test/GetStarted.md
@@ -0,0 +1,11 @@
+```
+npm install --global gulp-cli
+
+gulp --version
+```
+
+https://gulpjs.com/docs/en/getting-started/creating-tasks
+
+https://marketplace.visualstudio.com/items?itemName=ms-vscode.live-server
+
+`_name.scss` are partials, they are not compiled to css, but imported in the main index.scss. They are used to organize the code and make it more maintainable.
---
+++ gulp-test/gulpfile.js
@@ -0,0 +1,38 @@
+// Within this file, you'll often see gulp APIs, 
+// like src(), dest(), series(), or parallel() 
+// but any vanilla JavaScript or Node modules can be used. 
+// Any exported functions will be registered into gulp's task system.
+
+    // Public tasks are exported from your gulpfile, which allows them to be run by the gulp command.
+    // Private tasks are made to be used internally, usually used as part of series() or parallel() composition.
+
+const { series } = require('gulp');
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// The `clean` function is not exported so it can be considered a private task.
+// It can still be used within the `series()` composition.
+function clean(cb) {
+  // body omitted
+  cb();
+}
+
+// The `build` function is exported so it is public and can be run with the `gulp` command.
+// It can also be used within the `series()` composition.
+function build(cb) {
+  // body omitted
+  cb();
+}
+
+function buildStyles() {
+    return src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('public/css'))
+}
+
+function watchTask() {
+    watch(['sass/**/*.scss'], buildStyles);
+}
+
+exports.build = build;
+exports.default = series(buildStyles, watchTask);
